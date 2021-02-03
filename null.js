570d2534f52aba3e9d87e716fc5f8cd9537a75ec
haquejia <hemaljia0017@gmail.com>
2021-02-03 17:55:41 +0600

    null vs undefine

---
+++ null.js
@@ -0,0 +1,20 @@
+//let pakhi;
+//function add(num1, num2){
+ //   console.log(num1+num2);
+ //   return  ;
+//}
+
+//const result =add(13,82);
+//console.log(result);
+
+//function add (num1,num2){
+   // console.log(num1,num2)
+//}
+const result ={name:'hemal',phone:'012'};
+console.log(result.gf);
+
+let fun = undefined;
+console.log( fun);
+
+let age = [ 12, 13 , 15];
+console.log(age[12])
